8024eeccd9048f82eb4af3e2bc48bca15fe5ade9
jules <142106821+Jules-12-8@users.noreply.github.com>
2024-01-17 17:19:21 +0100

    Feat: add rainbow and shades drawing + buttons

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,6 @@
 const gridWidth = 600;
 const axisSize = 4;
+let drawStyle = "monochromatic";
 
 const rootElem = document.querySelector(":root");
 
@@ -32,37 +33,61 @@ function createGrid(size) {
 createGrid(axisSize);
 
 
-function createDrawListeners() {
-    const gridCells = document.querySelectorAll(".grid-cell");
-
-    gridCells.forEach((cell) => {
-        cell.addEventListener("mouseenter", () => {
-            cell.style.background = "coral"
-        });
-    });
-}
-
-createDrawListeners();
-
-
 function computeRandValue() {
     return Math.trunc(Math.random() * 256);
 }
 
 function getRandRgbValue() {
-    return "rgb("+computeRandValue()+","+computeRandValue()+","+computeRandValue()+")";
+    return "rgb(" + computeRandValue() + "," + computeRandValue() + "," + computeRandValue() + ")";
 }
 
-function createRandDrawListeners() {
+function darkenRgb(previousValue) {
+    let value = previousValue.slice(4, 7);
+    if (isNaN(value.at(1))) {
+        return previousValue;
+    } else if (isNaN(value.at(2))) {
+        value = value.slice(0, 2);
+        value = +value - 21;
+    } else {
+        value = +value - 21;
+    }
+    return `rgb(${value}, ${value}, ${value})`;
+}
+    
+
+function createDrawListeners(drawStyle) {
     const gridCells = document.querySelectorAll(".grid-cell");
 
-    gridCells.forEach((cell) => {
-        cell.addEventListener("mouseenter", () => {
-            cell.style.background = getRandRgbValue();
-        });
-    });
+    switch (drawStyle) {
+        case "monochromatic":
+            gridCells.forEach((cell) => {
+                cell.addEventListener("mouseenter", () => {
+                    cell.style.background = "coral"
+                });
+            });
+            break;
+        case "rainbow":
+            gridCells.forEach((cell) => {
+                cell.addEventListener("mouseenter", () => {
+                    cell.style.background = getRandRgbValue();
+                });
+            });
+            break;
+        case "shades":
+            gridCells.forEach((cell) => {
+                cell.addEventListener("mouseenter", () => {
+                    const style = window.getComputedStyle(cell);
+                    cell.style.background = darkenRgb(style.getPropertyValue("background-color"));
+                });
+            });
+            break;
+    }
 }
 
+createDrawListeners(drawStyle);
+
+
+//UI
 
 const resetButton = document.querySelector("#reset");
 
@@ -72,12 +97,28 @@ resetButton.addEventListener("click", () => {
     gridCells.forEach((cell) => cell.style.background = "lightgrey");
 });
 
+const monochromaticButton = document.querySelector("#monochromatic");
+monochromaticButton.addEventListener("click", () => {
+    drawStyle = "monochromatic";
+    changeParameters();
+});
+
+const rainbowButton = document.querySelector("#rainbow");
+rainbowButton.addEventListener("click", () => {
+    drawStyle = "rainbow";
+    changeParameters();
+});
+
+const shadesButton = document.querySelector("#shades");
+shadesButton.addEventListener("click", () => {
+    drawStyle = "shades";
+    changeParameters();
+});
 
 const sizeControl = document.querySelector("#size-control");
 
 //Populate the select element with the options
-for (let i = 4; i <= 124; i = (i*1.5 + (i*1.5%2))) {
-    // console.log(i);
+for (let i = 4; i <= 124; i = (i * 1.5 + (i * 1.5 % 2))) {
     const option = document.createElement("option");
 
     option.value = i;
@@ -86,20 +127,22 @@ for (let i = 4; i <= 124; i = (i*1.5 + (i*1.5%2))) {
 }
 
 
+function changeParameters() {
+    const cellsDivs = document.querySelectorAll("div[class^='column']");
+
+    cellsDivs.forEach((col) => {
+        col.remove();
+    });
+
+    const size = sizeControl.value;
+
+    setCellSize(gridWidth, size);
+    createGrid(size);
+    createDrawListeners(drawStyle);
+}
+
 const sizeOption = document.querySelectorAll("option");
 
 sizeOption.forEach((option) => {
-    option.addEventListener("click", () => {
-        const cellsDivs = document.querySelectorAll("div[class^='column']");
-
-        cellsDivs.forEach((col) => {
-            col.remove();
-        });
-
-        const size = sizeControl.value;
-
-        setCellSize(gridWidth, size);
-        createGrid(size);
-        createDrawListeners();
-    });
-})
+    option.addEventListener("click", () => changeParameters());
+});
